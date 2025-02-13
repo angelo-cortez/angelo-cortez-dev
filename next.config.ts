ab3de2e1780749947d07f1eee5ebbbfec4608c49
angelo-cortez <angelo.cortez@8x8.com>
2025-02-13 13:50:57 +0800

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/angelo-cortez-dev', // Replace with your repository name
   images: {
     unoptimized: true,
   },
