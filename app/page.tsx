0e941456c349944b73d2ebff97f5e81c55cac3f6
angelo-cortez <angelo.cortez@8x8.com>
2025-02-13 13:29:56 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
           I started off my career as a solutions engineer at Epic, where I did development on their risk of sepsis model for hospitals. Asynchronously, I started my masters in CS @ UW Madison, where I worked under Professor Dane Morgan working on materials machine learning research.
           <br />
           <br />
-          After three years at Epic, I joined 8x8 where I’ve built UCaaS and CCaaS automations for SMBs and enterprises, creating integrations that streamline operations and drive efficiency for customers' contact centers. I also collaborate with customers on custom routing configurations to enhance call flow, automate interactions, and improve customer experience.
+          After three years at Epic, I joined 8x8 as a software engineer where I’ve built UCaaS and CCaaS automations for SMBs and enterprises, creating integrations that streamline operations and drive efficiency for customers' contact centers. I also collaborate with customers on custom routing configurations to enhance call flow, automate interactions, and improve customer experience.
           <br />
           <br />
           My current interests are applications of AI in tax, finance, and real estate.
